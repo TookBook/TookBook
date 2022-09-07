23ea80abbcbcfbd6c315abcf158fe3da099ab61e
Jesper Eriksson <89935390+jes8eri@users.noreply.github.com>
2022-09-07 09:18:13 +0200

    Basic backend filestructure

---
+++ TookBook/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace TookBook.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Book
+    {
+
+    }
+}
