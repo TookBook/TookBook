df8883bd7dc58141b181dfea90f1cf2a703370d7
TiiaTu <tiia.tupamaki@gmail.com>
2022-09-09 11:18:38 +0200

    ny test

---
+++ TookBook/Controllers/BookController.cs
@@ -0,0 +1,37 @@
+﻿
+namespace TookBook.Controllers
+{
+    using System;
+    using Microsoft.AspNetCore.Mvc;
+    using TookBook.Services;
+    using TookBook.Models;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookController : ControllerBase
+    {
+        private readonly BookService _bookService; //TODO: lägg till alla services
+
+        public BookController(BookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Book>>> Get() {
+            var books = await _bookService.GetAsync();
+            if (books == null)
+                return NotFound();
+            return Ok(books);
+        }
+
+        //[HttpPost]
+        //public async Task<IActionResult> Post([FromBody] Book book) { }
+        
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> AddToBooks(string id, [FromBody] string bookId) { }
+
+        //[HttpDelete("{id}")]
+        //public async Task<IActionResult> Delete(string id) { }
+    };
+}
