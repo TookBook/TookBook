dab2a3bdc18755d633f0006ee2cd8de8a08e5eb8
TiiaTu <tiia.tupamaki@gmail.com>
2022-09-12 09:44:52 +0200

    nu fungerar den!

--- TookBook/Models/Book.cs
+++ TookBook/Models/Book.cs
@@ -12,29 +12,36 @@ using MongoDB.Bson;
 
     public class Book : IBook
     {
-
-        
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
+        
         public string BookId { get; set; } = "";
 
+        [BsonElement("title")]
         public string Title { get; set; } = "";
-
-
+        
+        [BsonElement("category")]
         public string Category { get; set; } = "";
-
+        
+        [BsonElement("language")]
         public string Language { get; set; } = "";
-
+        
+        [BsonElement("authors")]
         public string[]? Authors { get; set; }
-
+        
+        [BsonElement("year")]
         public int Year { get; set; }
-
+        
+        [BsonElement("inStock")]
         public int[]? InStock { get; set; }
-
+        
+        [BsonElement("price")]
         public decimal Price { get; set; }
-
+        
+        [BsonElement("seller")]
         public string Seller { get; set; } = "";
-
+        
+        [BsonElement("bookInfo")]
         public string BookInfo { get; set; } = "";
     }
 }
