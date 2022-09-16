a15e4e65ff2cee52dee4e0899316a664fba7386a
TiiaTu <tiia.tupamaki@gmail.com>
2022-09-16 09:55:07 +0200

    grundkoden för varukorgsidan

--- TookBook/ClientApp/src/pages/shoppingCart.jsx
+++ TookBook/ClientApp/src/pages/shoppingCart.jsx
@@ -1,1 +1,22 @@
-//detta är test
+
+
+const ShoppingCart = () => {
+	  return (
+	<div className="main-container">
+		<h1>Your shopping cart</h1>
+		<div className="book-info">
+			<p>book title here</p>
+			<p>authors name here</p>
+			<p>-</p>
+			<p>2</p>
+			<p>+</p>
+			</div>
+
+		<div className="book-price">
+			<p>xx SEK</p>
+			</div>
+	  
+	</div>
+  );
+}
+export default ShoppingCart;
