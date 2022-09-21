69343408aac8c2457fb3c9875e35939a9174ac8d
Jesper Eriksson <89935390+jes8eri@users.noreply.github.com>
2022-09-21 11:20:19 +0200

    Added recoil, added userPortal modal (no func)

--- TookBook/ClientApp/src/App.jsx
+++ TookBook/ClientApp/src/App.jsx
@@ -5,27 +5,24 @@ import CssBaseline from '@mui/material/CssBaseline'
 import Theme from "./style/MuiTheme"
 import ThemeWrapper from './style/ThemeWrapper'
 import { Box, Typography } from '@mui/material'
+import UserLoginContainer from "./components/userPortalModal/UserLoginContainer"
+import { RecoilRoot } from 'recoil'
 
 function App() {
   const [count, setCount] = useState(0)
 
 
-  const getWeather = async () => {
-    // const resp = await fetch("/api/weatherforecast")
-    // const json = await resp.json()
-    // console.log(json)
-    // setWeather(json);
-  }
 
   /** ThemeWrapper är en komponent som ligger i style/Themewrapper, där hämtar den ett tema för hela material-ui från style/MuiTheme.
    *  I MuiTheme så kan man ändra alla färger, ex primary som används nedan. Använder man "primary.main" så får man den färgen man döpte till primary, använder man primary.light så får man automatiskt en ljusare variant av färgen. Likadant med primary.dark  */
   return (
     <ThemeWrapper>
-      <Navbar />
+      <RecoilRoot>
+        <Navbar />
+        <UserLoginContainer></UserLoginContainer>
 
-
-      {/** Exempel på MaterialUI :  */}
-      {/**<>
+        {/** Exempel på MaterialUI :  */}
+        {/**<>
       <Box sx={{ bgcolor: "primary.main" }}>
         <Typography> Hello World </Typography>
       </Box>
@@ -39,8 +36,8 @@ function App() {
       </Box>
       </>*/}
 
-      {/** Precis samma som nedan, men <Typography> har inte default margin/padding som <p> :  */}
-      {/**<>
+        {/** Precis samma som nedan, men <Typography> har inte default margin/padding som <p> :  */}
+        {/**<>
       <div style={{ backgroundColor: "lightblue" }}>
         <p>Hello World </p>
       </div>
@@ -48,7 +45,7 @@ function App() {
         <p>Hello World </p>
       </div>
       </> */}
-
+      </RecoilRoot>
     </ThemeWrapper>
   )
 }
---
+++ TookBook/ClientApp/src/components/userPortalModal/UserLoginContainer.jsx
@@ -0,0 +1,98 @@
+import { useRecoilState } from "recoil"
+import { useState, forwardRef } from "react";
+import Slide from '@mui/material/Slide';
+import Box from "@mui/material/Box";
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import Chip from '@mui/material/Chip';
+import Grid from '@mui/material/Grid';
+import Collapse from '@mui/material/Collapse';
+import Dialog from "@mui/material/Dialog";
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Paper from "@mui/material/Paper"
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import openUserPortalState from "../../atoms/openUserPortalState";
+import LoginForm from "./LoginForm";
+import SignUpForm from "./SignUpForm";
+
+
+// Slide animation from Mui.
+const Transition = forwardRef(function Transition(props, ref) {
+	return <Slide direction="down" ref={ref} {...props} />;
+});
+
+// Boilerplate code from Mui-Tabs to get tabs to work. see https://mui.com/material-ui/react-tabs/ for info.
+function TabPanel(props) {
+	const { children, value, index, ...other } = props;
+
+	return (
+		<div
+			role="tabpanel"
+			hidden={value !== index}
+			id={`simple-tabpanel-${index}`}
+			aria-labelledby={`simple-tab-${index}`}
+			{...other}
+		>
+			{value === index && (
+				<Box sx={{ p: 3 }}>
+					<Box>{children}</Box>
+				</Box>
+			)}
+		</div>
+	);
+}
+
+const UserPortalContainer = () => {
+	const [openContainer, setOpenContainer] = useRecoilState(openUserPortalState)
+	const [tabValue, setTabValue] = useState(1);
+
+	const handleClose = () => {
+		setOpenContainer(false);
+	}
+
+	const handleTabChange = (e, newValue) => {
+		setTabValue(newValue)
+	}
+
+	// TODO: Conditional rendering based on whether user is logged in or not, userloggedinstate
+	return (
+		<Dialog
+
+			scroll="paper"
+
+			fullWidth={true}
+			maxWidth={"xs"}
+
+			open={openContainer}
+			TransitionComponent={Transition}
+			sx={{ minWidth: "100%" }}
+			keepMounted
+			onClose={handleClose}
+			BackdropProps={{ style: { backgroundColor: "rgba(0,0,0,0.1)" } }}
+		>
+			<Paper sx={{ minHeight: "550px" }} >
+
+				<Tabs centered value={tabValue} onChange={handleTabChange}>
+					<Tab label="Login" value={1} />
+					<Tab label="Register" value={2} />
+				</Tabs>
+
+				<TabPanel value={tabValue} index={1}>
+					<LoginForm />
+				</TabPanel>
+
+				<TabPanel value={tabValue} index={2}>
+					<SignUpForm />
+				</TabPanel>
+
+			</Paper>
+		</Dialog>
+	)
+}
+
+export default UserPortalContainer
