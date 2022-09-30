f092ff60791a1124ede803ac6c2374e2ceb49338
Jesper Eriksson <89935390+jes8eri@users.noreply.github.com>
2022-09-30 17:02:53 +0200

    Removed unnecesary usings

--- TookBook/ClientApp/src/pages/homepage.jsx
+++ TookBook/ClientApp/src/pages/homepage.jsx
@@ -1,9 +1,6 @@
 import Container from '@mui/material/Container';
-import { DictionaryClichesSlide, TeenHorrorSlide, HitchhikersGuideSlide, SwedenSlide, UsedBookStoreSlide, NumbersRaiseSlide } from '../assets/images/index';
-import Carousel from "react-multi-carousel"
 import Box from "@mui/material/Box";
 import CarouselSlides from '../components/CarouselSlides';
-import "react-multi-carousel/lib/styles.css";
 import Typography from '@mui/material/Typography';
 
 
