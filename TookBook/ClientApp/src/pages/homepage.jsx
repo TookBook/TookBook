d8f88044948996b24785454eaa3b090c919827a0
Jesper Eriksson <89935390+jes8eri@users.noreply.github.com>
2022-09-30 17:00:51 +0200

    Added new carousel package, added img assets

--- TookBook/ClientApp/src/pages/homepage.jsx
+++ TookBook/ClientApp/src/pages/homepage.jsx
@@ -1,13 +1,21 @@
 import Container from '@mui/material/Container';
-
+import { DictionaryClichesSlide, TeenHorrorSlide, HitchhikersGuideSlide, SwedenSlide, UsedBookStoreSlide, NumbersRaiseSlide } from '../assets/images/index';
+import Carousel from "react-multi-carousel"
+import Box from "@mui/material/Box";
+import CarouselSlides from '../components/CarouselSlides';
+import "react-multi-carousel/lib/styles.css";
+import Typography from '@mui/material/Typography';
 
 
 
 const Homepage = () => {
 
 	return (
-		<Container>
-			<div>Homeplaceholder</div>
+		<Container >
+
+			<Typography align='center'>Homeplaceholder woo</Typography>
+
+			<CarouselSlides />
 
 		</Container>
 	)
