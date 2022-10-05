86ee5cf9d47c65ff919e6cb8f784c10cd3eec1ac
Max <Max@Home>
2022-10-05 15:05:16 +0200

    admin recoil users data

--- TookBook/ClientApp/src/pages/AdminMenu.jsx
+++ TookBook/ClientApp/src/pages/AdminMenu.jsx
@@ -5,34 +5,34 @@ import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import Users from '../../../userSeedData.json'
-
+import { useRecoilState } from 'recoil';
+import { fetchedUsersState } from '../atoms';
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import Button from '@mui/material/Button';
-
 import Checkbox from '@mui/material/Checkbox';
-
-import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 
+
+
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  pt: 2,
-  px: 4,
-  pb: 3,
+	position: 'absolute',
+	top: '50%',
+	left: '50%',
+	transform: 'translate(-50%, -50%)',
+	width: 400,
+	bgcolor: 'background.paper',
+	border: '2px solid #000',
+	boxShadow: 24,
+	pt: 2,
+	px: 4,
+	pb: 3,
 };
 
 function ChildModal() {
+	
 
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => {
@@ -71,8 +71,10 @@ function ChildModal() {
 
   
 const AdminMenu = () => {
+	const [Users, setUsers] = useRecoilState(fetchedUsersStateValue)
 	const [userType, setUserType] = React.useState('');
 	const [open, setOpen] = React.useState(false);
+	console.log(Users);
 	const handleOpen = () => {
 	  setOpen(true);
 	};
