fa6035a5e686484ad690a6ab3b2c93d4d626068f
TiiaTu <tiia.tupamaki@gmail.com>
2022-10-05 15:30:17 +0200

    .

--- TookBook/ClientApp/src/pages/shoppingCart.jsx
+++ TookBook/ClientApp/src/pages/shoppingCart.jsx
@@ -1,42 +1,35 @@
-import "../style/shoppingCart.css";
+// import "../style/shoppingCart.css";
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 
-const style = {
-	position: 'absolute',
-	top: '50%',
-	left: '50%',
-	transform: 'translate(-50%, -50%)',
-	width: 400,
-	height: 600,
-	bgcolor: 'background.paper',
-	border: '2px solid #000',
-	boxShadow: 24,
-	pt: 2,
-	px: 4,
-	pb: 3,
-  };
+// const style = {
+// 	position: 'absolute',
+// 	top: '50%',
+// 	left: '50%',
+// 	transform: 'translate(-50%, -50%)',
+// 	width: 400,
+// 	height: 600,
+// 	bgcolor: 'background.paper',
+// 	border: '2px solid #000',
+// 	boxShadow: 24,
+// 	pt: 2,
+// 	px: 4,
+// 	pb: 3,
+//   };
 
 const ShoppingCart = () => {
 	return (
-		<Container maxWidth="md" >
-			<Box style={{backgroundcolor: '#ab2b06'}} display={"flex"} justifyContent={"center"} 
-				sx={{
-					width: 400,
-					height: 300,
-					backgroundColor: '#f2f2f2',
-					borderRadius: 1,
-				}}
-			/>
-			<Typography align='center'>Shopping Cart</Typography>
-			<Box marginTop={"3rem"}>
-				
-			</Box>
-			
+		<Container>
+		<Box sx={{
+			maxHeight: "400px", borderRadius: "0.5rem",
+			"&:hover": { transition: "all .2s ease-in-out", transform: "scale(1.1)", boxShadow: "5px 20px 10px black", }
+		}}>
+		</Box>
 		</Container>
 	)
 }
+
 // 	  return (
 // 	<div className="main-container">
 // 		<h1>Your shopping cart</h1>
