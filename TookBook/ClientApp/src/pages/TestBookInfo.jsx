7893934bbf0fa6b9ab664edeafe9788da8a91089
Jesper Eriksson <89935390+jes8eri@users.noreply.github.com>
2022-10-08 19:39:34 +0200

    New image container, removed mui-image

--- TookBook/ClientApp/src/pages/TestBookInfo.jsx
+++ TookBook/ClientApp/src/pages/TestBookInfo.jsx
@@ -1,23 +1,38 @@
 import "../style/bookInfo.css";
 import { useParams } from "react-router-dom";
 import { fetchedBooksState } from "../atoms";
-import Image from '@jy95/material-ui-image'
+import Image from "mui-image";
 import { useRecoilValue } from "recoil";
 import { minHeight } from "@mui/system";
+import { useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
+
 
 const lorem = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentiumoptio, eaque rerum! Provident similique accusantium nemo autem. Veritatisobcaecati tenetur iure eius earum ut molestias architecto voluptate aliquamnihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdamrecusandae alias error harum maxime adipisci amet laborum. Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur fugiat, temporibus enim commodi iusto libero magni deleniti quod quam consequuntur! Commodi minima excepturi repudiandae velit hic maximedoloremque. Quaerat provident commodi consectetur veniam similique ad earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto ab laudantiummodi minima sunt esse temporibus sint culpa, recusandae aliquam numquam totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam quasi aliquam eligendi, placeat qui corporis!';
 
 const TestBookInfo = () => {
 	const fetchedBooks = useRecoilValue(fetchedBooksState)
+	const navigate = useNavigate()
 
 	const { id } = useParams()
 	const book = fetchedBooks.find(book => book.bookId === id)
 
 
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [])
+
+	useEffect(() => {
+		// if (book?.bookId == null) navigate("/")
+		console.log(book)
+	}, [book], [id])
+
+
+	// TODO: Crash when refreshing page since it doesn't get any info rom useParams, intercept and redirect?
 	return (
 		<div className="book-info-container">
 			{/* <p className="picture"></p> */}
-			<Image className="picture" style={{ marginRight: "10px", objectFit: "contain", minHeight: "500px" }} src={book.imgUrl} />
+			<Image shift="right" duration={1000} className="picture" style={{ marginRight: "10px", objectFit: "contain", minHeight: "500px" }} src={book.imgUrl} />
 			<div className="book-info">
 				<h2>{book.title}</h2>
 				<ul>
