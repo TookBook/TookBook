45e6213570184aa7ffdbebe3ffa907c776edaa9e
Jesper Eriksson <89935390+jes8eri@users.noreply.github.com>
2022-10-12 14:44:45 +0200

    Disables cart button if user is not logged in

--- TookBook/ClientApp/src/pages/shoppingCart.jsx
+++ TookBook/ClientApp/src/pages/shoppingCart.jsx
@@ -4,29 +4,30 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import shoppingCartContentsState from '../atoms/shoppingCartContents';
-import { useRecoilState, useRecoilValue} from 'recoil';
+import { isUserLoggedInState } from '../atoms';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import BookInCart from '../components/shoppingCart/BookInCart';
 import { Link } from 'react-router-dom';
 
 const ShoppingCart = () => {
 
 	const [itemsInCart, setItemsInCart] = useRecoilState(shoppingCartContentsState);
-	
+	const userLoggedInStatus = useRecoilValue(isUserLoggedInState)
 
 	const handleRemoveFromCart = (book) => {
 		const exists = itemsInCart.find((x) => x.id === book.id);
 		console.log(exists)
-		if(exists.amount === 1) {
+		if (exists.amount === 1) {
 			setItemsInCart(itemsInCart.filter((x) => x.id !== book.id));
 		} else {
-			setItemsInCart(itemsInCart.map((x) => x.id === book.id ? {...exists, amount: exists.amount - 1} : x));
+			setItemsInCart(itemsInCart.map((x) => x.id === book.id ? { ...exists, amount: exists.amount - 1 } : x));
 		}
 	};
 
 
 	// function handleAddToCart(id) {
 	// 	let exists = itemsInCart.find(item => item.id === id);
-		
+
 	// 	setItemsInCart(currItems => {
 	// 		if (exists == null) {
 	// 			return [...currItems, { id, amount: 1 }]
@@ -41,11 +42,11 @@ const ShoppingCart = () => {
 	// 		}
 	// 	})
 	// 	console.log("added to cart")
-		
+
 	// }
 
 	const handleAddToCartTwo = (book) => {
-		
+
 
 		setItemsInCart([...itemsInCart, book])
 		console.log("Items in cart:", itemsInCart)
@@ -66,7 +67,6 @@ const ShoppingCart = () => {
 
 	return (
 		<Container>
-
 			{itemsInCart.length === 0 ? <p>No items in cart.</p> : null}
 			{itemsInCart.map((book) => (
 				<BookInCart
@@ -76,9 +76,10 @@ const ShoppingCart = () => {
 					increaseAmountInCart={(book) => handleAddToCartTwo(book)}
 				/>
 			))}
-			<Link to="/checkout">
-			<Button sx={{mt:4}} variant="contained">Proceed to pay</Button>
-			</Link>
+
+			<Button disabled={!userLoggedInStatus} sx={{ mt: 4 }} variant="contained"> <Link style={{ color: "white", textDecoration: "none" }} to="/checkout">Proceed to pay</Link></Button>
+
+			{!userLoggedInStatus && <Typography>Please log in before proceeding to checkout.</Typography>}
 		</Container>
 	)
 }
